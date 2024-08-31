2c11c59a001787ed5e64d1d04cc988778fbc4647
higu <ilya-isakov-2018@bk.com>
2024-09-01 01:33:29 +0300

    c

--- main.go
+++ main.go
@@ -10,6 +10,20 @@ import (
 	"sort"
 )
 
+type config struct {
+	token   string
+	public  string
+	session string
+}
+
+func New() config {
+	var conf config
+	conf.token = "-n-13WmyQgHl1TbN7rISCUAR7Xv6Pewy5LxhdWwOO9pIOX1OSTsNEwUONcPuJx4qqZxf1Y2eIFUSb1CZysx0"
+	conf.public = "CBMFPMLGDIHBABABA"
+	conf.session = "9876f792b60b2738f751319407a0dcca"
+	return conf
+}
+
 func arr_in_str(params map[string]string) string {
 	var stringi string
 	keys := make([]string, 0, len(params))
@@ -39,6 +53,7 @@ func main() {
 		"attachment":      `{"media": [{ "type": "text", "text": "This is a text of a new topic" }]}`,
 		"format":          "json",
 	}
+
 	g := fmt.Sprint(`https://api.ok.ru/fb.do?application_key=CBMFPMLGDIHBABABA&attachment=%7B%22media%22%3A%20%5B%7B%20%22type%22%3A%20%22text%22%2C%20%22text%22%3A%20%22This%20is%20a%20text%20of%20a%20new%20topic%22%20%7D%5D%7D&format=json&gid=55962941456457&method=mediatopic.post&type=GROUP_THEME&sig=98b627bfd2c2b6143b3d9e5602c5e92a&access_token=-n-13WmyQgHl1TbN7rISCUAR7Xv6Pewy5LxhdWwOO9pIOX1OSTsNEwUONcPuJx4qqZxf1Y2eIFUSb1CZysx0`)
 	sig := GetMD5Hash(arr_in_str(params) + conf.session)
 	params["access_token"] = conf.token
